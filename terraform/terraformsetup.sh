22f7189737bce1f5191ee4f7d2ed9ee595db2689
NoahKirchner <noahthomkirchner@gmail.com>
2023-05-11 13:29:40 -0400

    Fixes small type

--- terraform/terraformsetup.sh
+++ terraform/terraformsetup.sh
@@ -10,7 +10,7 @@ if ! command -v pveum &>/dev/null; then
     exit
 fi
 
-pveum role add TerraformProv - privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt"
+pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt"
 pveum user add terraform-prov@pve --password $password 
 pveum aclmod / -user terraform-prov@pve -role TerraformProv
 
